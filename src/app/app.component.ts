500ea3dcaf314fb3a4df128770b1289a99aec548
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-11 14:49:24 +0700

    Add progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  percent = 40;
   title = 'M5-W1-ProgressBar';
 }
---
+++ src/app/progressbar/progressbar.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-progressbar',
+  templateUrl: './progressbar.component.html',
+  styleUrls: ['./progressbar.component.css']
+})
+export class ProgressbarComponent implements OnInit {
+  @Input() backgroundColor = '#D9D9D9';
+  @Input() progressColor = '#4CAF50';
+  @Input() width = 50;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
